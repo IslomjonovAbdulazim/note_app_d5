ae8981610381e457d2221d3d643ac8eaaa4aae97
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-25 16:31:45 +0500

    imp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -24,10 +25,78 @@ class _HomePageState extends State<HomePage> {
     setState(() {});
   }
 
-  void save() async {}
+  void save() async {
+    String text = noteController.text.trim();
+    if (text.isEmpty) return;
+    notes.add(text);
+    final db = await SharedPreferences.getInstance();
+    await db.setStringList("notes", List<String>.from(notes));
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.yellow,
+        surfaceTintColor: Colors.yellow,
+        title: Text("Notes"),
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                TextField(
+                  controller: noteController,
+                  autocorrect: false,
+                  maxLines: null,
+                  maxLength: 200,
+                  textInputAction: TextInputAction.done,
+                ),
+                SizedBox(height: 20),
+                CupertinoButton(
+                  color: Colors.yellow,
+                  onPressed: () {
+                    save();
+                    noteController.clear();
+                  },
+                  child: Center(child: Text("Save")),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              itemCount: notes.length,
+              padding: EdgeInsets.all(20),
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(3),
+                  child: ListTile(
+                    tileColor: Colors.grey.shade200,
+                    title: Text(notes[index]),
+                    trailing: CupertinoButton(
+                      padding: EdgeInsets.zero,
+                      onPressed: () async {
+                        notes.remove(notes[index]);
+                        setState(() {});
+                        final db = await SharedPreferences.getInstance();
+                        await db.setStringList("notes", List<String>.from(notes));
+                      },
+                      child: Icon(CupertinoIcons.delete),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
